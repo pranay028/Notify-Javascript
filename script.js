39f0035212cb771f50cbc2dcdea33498936ebd9b
pranay028 <pranaynomula28@gmail.com>
2021-06-22 15:29:09 +0530

    local storage added and some styling changes

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const actionText = document.querySelector(".action-text");
 let editID;
 let editElement;
 
+document.addEventListener("DOMContentLoaded", getNotes);
 // ? adding Popup screen funtionality------
 
 addBtn.addEventListener("click", function () {
@@ -65,13 +66,18 @@ const addItem = function () {
   if (!text) return actionFunc("Empty Value");
   startText.classList.add("hidden");
   mainContainer.appendChild(element);
+  savelocalStorage(element);
   actionFunc("saved");
 };
 
 // ? Delete Funnction
 
 const deleteFunc = (ele) => {
+  const deleEle = ele.querySelector(".text").textContent;
+
   mainContainer.removeChild(ele);
+  deleteStorage(deleEle);
+
   if (mainContainer.childElementCount == 0) {
     startText.classList.remove("hidden");
   }
@@ -85,9 +91,11 @@ const editFunc = (textElement, id) => {
   const edit_save = editPop.querySelector(".edit");
   const textEdit = editPop.querySelector(".edit-input");
   const cancel = editPop.querySelector(".cancel");
+
   // console.log(textEdit);
   editID = id;
   textEdit.value = textElement.textContent;
+  const forStorageOld = textElement.textContent;
   textEdit.addEventListener("input", function () {
     textEdit.textContent = textEdit.value;
   });
@@ -97,15 +105,23 @@ const editFunc = (textElement, id) => {
   edit_save.addEventListener("click", function () {
     const getCont = document.getElementById(`${editID}`);
     // console.log(getCont);
+    // console.log(editID);
+    // console.log(getCont);
+    // console.log(mainContainer);
 
     const textEl = getCont.querySelector(".text");
-    // console.log(textEl);
+
     textEl.textContent = textEdit.innerHTML;
+    // console.log(textEl.textContent);
+
+    // console.log(forStorageOld, textEl.textContent);
 
     textEl.textContent =
       textEl.textContent == " " || textEl.textContent == ""
         ? "Empty Note - Can Delete"
         : textEl.textContent;
+
+    editStorage(forStorageOld, textEl.textContent);
     editPop.classList.add("hidden");
     actionFunc("edited");
   });
@@ -160,3 +176,70 @@ mainContainer.addEventListener("click", function (e) {
     }
   });
 });
+
+const savelocalStorage = function (note) {
+  // check do i have a previous array of items
+  let notes = [];
+  if (localStorage.getItem("notes") === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
+
+  const noteText = note.querySelector(".text");
+  const noteValue = noteText.textContent;
+  notes.push(noteValue);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  // console.log(noteValue);
+  // console.log(notes);
+};
+
+let notes;
+function getNotes() {
+  let currentID = new Date().getTime().toString();
+
+  if (localStorage.getItem("notes") === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
+
+  notes.forEach((note) => {
+    const element = document.createElement("div");
+    element.classList.add("each-container");
+    element.setAttribute("id", currentID);
+    // console.log(element);
+    const html = ` 
+        <div class="inner-container">
+          <p class="text">${note}</p>
+          <div class="icons">
+            <div class="edit-icon"
+              ><i class="fas fa-pencil-alt edit"></i
+            ></div>
+            <div class="edit-icon"><i class="fas fa-trash delete"></i></div>
+          </div>
+        </div>`;
+
+    element.innerHTML = html;
+
+    startText.classList.add("hidden");
+    mainContainer.appendChild(element);
+  });
+}
+
+const deleteStorage = function (note) {
+  notes = JSON.parse(localStorage.getItem("notes"));
+  // console.log(note);
+  notes.splice(notes.indexOf(note), 1);
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+const editStorage = function (old, newText) {
+  // console.log(old, newText);
+  notes = JSON.parse(localStorage.getItem("notes"));
+  const replaceValue = notes.indexOf(old);
+  // console.log(replaceValue);
+  notes[replaceValue] = newText;
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
